5ca0c24013ed5fbe8d470d1b168b2923445b8bae
Andrei Precup <andreiprecup69@gmail.com>
2018-10-13 22:32:54 +0300

    Header of table is now fixed.

--- src/app/notes-list/notes-list.component.ts
+++ src/app/notes-list/notes-list.component.ts
@@ -14,6 +14,16 @@ export class NotesListComponent implements OnInit {
 
   ngOnInit() {
     this.notes = [new Note('Rejected Pdf CISM that embarrassed the team. Saved the honor of the bit team', true),
+      new Note(Math.random().toString(36).substring(7), Math.floor(Math.random() * 100) % 2 === 0),
+      new Note(Math.random().toString(36).substring(7), Math.floor(Math.random() * 100) % 2 === 0),
+      new Note(Math.random().toString(36).substring(7), Math.floor(Math.random() * 100) % 2 === 0),
+      new Note(Math.random().toString(36).substring(7), Math.floor(Math.random() * 100) % 2 === 0),
+      new Note(Math.random().toString(36).substring(7), Math.floor(Math.random() * 100) % 2 === 0),
+      new Note(Math.random().toString(36).substring(7), Math.floor(Math.random() * 100) % 2 === 0),
+      new Note(Math.random().toString(36).substring(7), Math.floor(Math.random() * 100) % 2 === 0),
+      new Note(Math.random().toString(36).substring(7), Math.floor(Math.random() * 100) % 2 === 0),
+      new Note(Math.random().toString(36).substring(7), Math.floor(Math.random() * 100) % 2 === 0),
+      new Note(Math.random().toString(36).substring(7), Math.floor(Math.random() * 100) % 2 === 0),
       new Note('Ran a jenkins command without sending an email about it', false)];
   }
 
